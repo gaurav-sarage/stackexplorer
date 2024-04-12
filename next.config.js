7260a4f90f177bcf6c1dd6eb3e9035b65f809266
Gaurav Sarage <gauravsarage.1902@gmail.com>
2024-04-13 00:54:52 +0530

    Setup file & folder structure 🧵

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
+};
+
+module.exports = nextConfig;
---
+++ src/app/fonts.ts
@@ -0,0 +1,12 @@
+import { Inter, Montserrat } from 'next/font/google';
+
+export const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+export const montserrat = Montserrat({
+  subsets: ['latin'],
+  variable: '--font-montserrat',
+  display: 'swap',
+});
