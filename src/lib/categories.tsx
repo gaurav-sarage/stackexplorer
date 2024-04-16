c8e44f27c9ef53b37c45902147d7424114e89fbd
Gaurav Sarage <gauravsarage.1902@gmail.com>
2024-04-17 03:11:17 +0530

    ✨ Refactored Framer imports & category data consolidation

---
+++ src/lib/categories.tsx
@@ -0,0 +1,20 @@
+import { categoryData as catData7 } from '@/data/ai-ml-development';
+import { categoryData as catData3 } from '@/data/cross-platform-development';
+import { categoryData as catData4 } from '@/data/desktop-development';
+import { categoryData as catData5 } from '@/data/game-development';
+import { categoryData as catData2 } from '@/data/mobile-development';
+import { categoryData as catData1 } from '@/data/web-development';
+import { Category } from '@/types';
+
+export function getCategories(): any[] {
+    const data = {
+        catData1,
+        catData2,
+        catData3,
+        catData4,
+        catData5,
+        catData7,
+    };
+    const output = Object.values(data);
+    return output;
+};
