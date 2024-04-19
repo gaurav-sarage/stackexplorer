574f682d635878838208cbf74b6b2a7dd982e7cc
Gaurav Sarage <gauravsarage.1902@gmail.com>
2024-04-20 03:59:21 +0530

    ✨ Add footer component with navigation and subscription form

---
+++ src/app/footer.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Link from "next/link";
+
+import MainNav from "@/components/main-nav";
+import NewSubscriberForm from "@/components/new-subscriber-form";
+import { Button } from "@/components/ui/button";
+import { Mail } from "lucide-react";
+
+export default function Footer() {
+    return (
+        <footer className="z-10 border-t border-gray-200 bg-white/50 py-8 backdrop-blur-lg">
+            <div className="container w-full">
+                <div className="flex flex-col space-y-6 md:space-y-0 md:flex-row md:items-end md:justify-between w-full">
+                    <div className="space-y-8 flex flex-col">
+                        <MainNav />
+                        <p className="max-w-xs text-sm text-gray-500">
+                            Discover the right tools for your next project effortlessly, saving you hours of research time!
+                        </p>
+                    </div>
+                    <div className="md:max-w-sm w-full">
+                        <NewSubscriberForm />
+                    </div>
+                </div>
+                <div className="mt-10 border-t border-gray-900/10 pt-8">
+                    <p className="text-sm leading-5 text-gray-500">
+                        @ 2024 StackExplorer. All Rights Reserved.
+                    </p>
+                </div>
+            </div>
+        </footer>
+    );
+};
