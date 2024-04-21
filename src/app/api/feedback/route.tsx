a9060af3f4cfca8b54aed36f7170009807fcf090
Gaurav Sarage <gauravsarage.1902@gmail.com>
2024-04-22 02:39:08 +0530

    📧 Implemented feedback email feature

---
+++ src/app/api/feedback/route.tsx
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import { FeedbackEmail } from "@/components/feedback-email";
+import { smtpEmail, transporter } from "@/lib/nodemailer";
+import { render } from "@react-email/components";
+
+export async function POST(req: NextRequest, res: NextResponse) {
+    const body = await req.json();
+    const { email, feedback } = body;
+
+    const emailHtml = render(<FeedbackEmail email={email} feedback={feedback} />);
+
+    const options = {
+        from: smtpEmail,
+        to: smtpEmail,
+        subject: "StackExplorer Feedback!",
+        html: emailHtml,
+    };
+
+    try {
+        await transporter.sendMail(options);
+    } catch (error) {
+        console.error("Failed to send email: ", error);
+    }
+    return new Response("OK");
+};
