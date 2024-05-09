abb4ddbcba08f91d99b4a489a1b9d74148c4058b
Gaurav Sarage <gauravsarage.1902@gmail.com>
2024-05-10 02:46:21 +0530

    ℹ️ Add email notification for new subscribers

---
+++ src/app/api/subscriber/route.tsx
@@ -0,0 +1,25 @@
+import { NextRequest, NextResponse } from "next/server";
+import { NewSubscriberEmail } from "@/components/new-subscriber-email";
+import { smtpEmail, transporter } from "@/lib/nodemailer";
+import { render } from "@react-email/components";
+
+export async function POST(req: NextRequest, res: NextResponse) {
+    const body = await req.json();
+    const { email } = body;
+
+    const emailHtml = render(<NewSubscriberEmail email={email} />);
+
+    const options = {
+        from: smtpEmail,
+        to: smtpEmail,
+        subject: "StackExplorer New Subscriber!!",
+        html: emailHtml,
+    };
+
+    try {
+        await transporter.sendMail(options);
+    } catch (error) {
+        console.error("Failed to send email: ", error);
+    }
+    return new Response("OK");
+};
