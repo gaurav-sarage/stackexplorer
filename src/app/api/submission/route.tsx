b15e643ef17fe41f3c9236f944c8bb395b805b57
Gaurav Sarage <gauravsarage.1902@gmail.com>
2024-05-17 00:09:38 +0530

    �  Refactor: Consolidated email sending & new subscriber tool logic in API routes

--- src/app/api/submission/route.tsx
+++ src/app/api/submission/route.tsx
@@ -1,26 +1,27 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from 'next/server';
 
-import { SubmissionEmail } from "@/components/submission-email";
-import { smtpEmail, transporter } from "@/lib/nodemailer";
-import { render } from "@react-email/components";
+import { SubmissionEmail } from '@/components/submission-email';
+import { smtpEmail, transporter } from '@/lib/nodemailer';
+import { render } from '@react-email/components';
 
 export async function POST(req: NextRequest, res: NextResponse) {
-    const body = await req.json();
-    const { category, url } = body;
+  const body = await req.json();
+  const { category, url } = body;
 
-    const emailHtml = render(<SubmissionEmail category={category} url={url} />);
+  const emailHtml = render(<SubmissionEmail category={category} url={url} />);
 
-    const options = {
-        from: smtpEmail,
-        to: smtpEmail,
-        subject: "You got a new submission on StackExplorer!",
-        html: emailHtml,
-    };
+  const options = {
+    from: smtpEmail,
+    to: smtpEmail,
+    subject: 'StackExplorer Submission!',
+    html: emailHtml,
+  };
 
-    try {
-        await transporter.sendMail(options);
-    } catch (error) {
-        console.error("Failed to send email: ", error);
-    }
-    return new Response("OK");
-};
+  try {
+    // Send email using the transporter
+    await transporter.sendMail(options);
+  } catch (error) {
+    console.error('Failed to send email:', error);
+  }
+  return new Response('OK');
+}
--- src/app/api/subscriber/route.tsx
+++ src/app/api/subscriber/route.tsx
@@ -1,25 +1,27 @@
-import { NextRequest, NextResponse } from "next/server";
-import { NewSubscriberEmail } from "@/components/new-subscriber-email";
-import { smtpEmail, transporter } from "@/lib/nodemailer";
-import { render } from "@react-email/components";
+import { NextRequest, NextResponse } from 'next/server';
+
+import { NewSubscriberEmail } from '@/components/new-subscriber-email';
+import { smtpEmail, transporter } from '@/lib/nodemailer';
+import { render } from '@react-email/components';
 
 export async function POST(req: NextRequest, res: NextResponse) {
-    const body = await req.json();
-    const { email } = body;
+  const body = await req.json();
+  const { email } = body;
 
-    const emailHtml = render(<NewSubscriberEmail email={email} />);
+  const emailHtml = render(<NewSubscriberEmail email={email} />);
+ 
+  const options = {
+    from: smtpEmail,
+    to: smtpEmail,
+    subject: 'StackExplorer New Subscriber!',
+    html: emailHtml,
+  };
 
-    const options = {
-        from: smtpEmail,
-        to: smtpEmail,
-        subject: "StackExplorer New Subscriber!!",
-        html: emailHtml,
-    };
-
-    try {
-        await transporter.sendMail(options);
-    } catch (error) {
-        console.error("Failed to send email: ", error);
-    }
-    return new Response("OK");
-};
+  try {
+    // Send email using the transporter
+    await transporter.sendMail(options);
+  } catch (error) {
+    console.error('Failed to send email:', error);
+  }
+  return new Response('OK');
+}
