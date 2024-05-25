01b15d2e0b14c359d42a902bdd75f3a827d16410
Gaurav Sarage <gauravsarage.1902@gmail.com>
2024-05-26 02:24:02 +0530

    ✨ Enhance Template component with framer motion animation

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,9 +1,9 @@
-import { MotionDiv } from "@/lib/framer";
+import { MotionDiv } from '@/lib/framer';
 
 export default function Template({ children }: { children: any }) {
-    return (
-        <MotionDiv initial={{ opacity: 0, y: 40 }} animate={{ opacity: 1, y: 0 }}>
-            {children}
-        </MotionDiv>
-    );
-};
+  return (
+    <MotionDiv initial={{ opacity: 0, y: 40 }} animate={{ opacity: 1, y: 0 }}>
+      {children}
+    </MotionDiv>
+  );
+}
