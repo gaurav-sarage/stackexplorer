47b1a9242cac418aedef833a8e6259b41cf1785e
Gaurav Sarage <gauravsarage.1902@gmail.com>
2024-05-26 22:57:02 +0530

    📧 feat: Add FeedbackEmail component for handling user feedback

--- src/components/feedback-email.tsx
+++ src/components/feedback-email.tsx
@@ -1,45 +1,44 @@
-import React from "react";
+import React from 'react';
 
 import {
-    Body,
-    Container,
-    Head,
-    Heading,
-    Html,
-    Tailwind,
-    Text
-} from "@react-email/components";
+  Body,
+  Container,
+  Head,
+  Heading,
+  Html,
+  Tailwind,
+  Text,
+} from '@react-email/components';
 
 interface FeedbackEmailProps {
-    email: string;
-    feedback: string;
-};
+  email: string;
+  feedback: string;
+}
 
 export const FeedbackEmail: React.FC<Readonly<FeedbackEmailProps>> = ({
-    email,
-    feedback
+  email,
+  feedback,
 }) => {
-    return (
-        <Html>
-            <Head />
-            <Tailwind>
-                <Body className="mx-auto my-auto bg-white font-sans">
-                    <Container className="mx-auto my-[40px] w-[465px] rounded border border-solid border-[#eaeaea] p-[20px]">
-                        <Heading className="mx-0 my-[30px] p-0 text-center text-[24px] font-normal text-black">
-                            You got a new message!
-                        </Heading>
-                        <Text className="text-[14px] leading-[24px] text-black">
-                            Hello Gaurav,
-                        </Text>
-                        <Text className="text-[14px] leading-[24px] text-black">
-                            You got an email from <strong>
-                                {email}
-                            </strong>.
-                            <br />
-                        </Text>
-                    </Container>
-                </Body>
-            </Tailwind>
-        </Html>
-    );
+  return (
+    <Html>
+      <Head />
+      <Tailwind>
+        <Body className="mx-auto my-auto bg-white font-sans">
+          <Container className="mx-auto my-[40px] w-[465px] rounded border border-solid border-[#eaeaea] p-[20px]">
+            <Heading className="mx-0 my-[30px] p-0 text-center text-[24px] font-normal text-black">
+              You got a message!
+            </Heading>
+            <Text className="text-[14px] leading-[24px] text-black">
+              Hello Hosna,
+            </Text>
+            <Text className="text-[14px] leading-[24px] text-black">
+              You got an email from <strong>{email}</strong>. <br />
+              Their feedback: <br />
+              {feedback}
+            </Text>
+          </Container>
+        </Body>
+      </Tailwind>
+    </Html>
+  );
 };
