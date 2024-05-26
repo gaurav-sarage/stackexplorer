2a6494f528b28d5c97681727e864be1358a3ff56
Gaurav Sarage <gauravsarage.1902@gmail.com>
2024-05-27 00:08:40 +0530

    🔰 Add MainNav component with LogoIcon and StackExplorer branding

--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -1,21 +1,20 @@
-import React from "react";
-import Image from "next/image";
-import Link from "next/link";
+import React from 'react';
 
-import { LogoIcon } from "./icons/logo";
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { LogoIcon } from './icons/logo';
 
 export default function MainNav() {
-    return (
-        <Link
-            href="/"
-            className="flex space-x-1.5 focus:outline-none focus-visible:ring-2 items-center"
-        >
-            <LogoIcon size={30} />
-            <span className="font-bold leading-3">
-                Stack<span className="text-[#3b82f6]">
-                    Explorer
-                </span>
-            </span>
-        </Link>
-    );
-};
+  return (
+    <Link
+      href="/"
+      className="flex space-x-1.5 focus:outline-none focus-visible:ring-2 items-center"
+    >
+      <LogoIcon size={30} />
+      <span className="font-bold leading-3">
+        Stack<span className="text-[#3b82f6]">Explorer</span>
+      </span>
+    </Link>
+  );
+}
