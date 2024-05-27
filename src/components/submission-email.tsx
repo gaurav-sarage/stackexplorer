5be9f1d6cbb2ea2e87df4ebfb7017132201884e2
Gaurav Sarage <gauravsarage.1902@gmail.com>
2024-05-27 19:19:21 +0530

    📨 feat: Add submission email template with category and URL

--- src/components/submission-email.tsx
+++ src/components/submission-email.tsx
@@ -1,45 +1,45 @@
-import React from "react";
+import React from 'react';
 
 import {
-    Body,
-    Container,
-    Head,
-    Heading,
-    Html,
-    Tailwind,
-    Text
-} from "@react-email/components";
+  Body,
+  Container,
+  Head,
+  Heading,
+  Html,
+  Tailwind,
+  Text,
+} from '@react-email/components';
 
 interface SubmissionEmailProps {
-    category: string;
-    url: string;
-};
+  category: string;
+  url: string;
+}
 
 export const SubmissionEmail: React.FC<Readonly<SubmissionEmailProps>> = ({
-    category,
-    url
+  category,
+  url,
 }) => {
-    return (
-        <Html>
-            <Head />
-            <Tailwind>
-                <Body className="mx-auto my-auto bg-white font-sans">
-                    <Container className="mx-auto my-[40px] w-[465px] rounded border border-solid border-[#eaeaea] p-[20px]">
-                        <Heading className="mx-0 my-[30px] p-0 text-center text-[24px] font-normal text-black">
-                            You got a new message!
-                        </Heading>
-                        <Text className="text-[14px] leading-[24px] text-black">
-                            Hello Gaurav,
-                        </Text>
-                        <Text className="text-[14px] leading-[24px] text-black">
-                            Category: {category}
-                        </Text>
-                        <Text className="text-[14px] leading-[24px] text-black">
-                            Tech URL: {url}
-                        </Text>
-                    </Container>
-                </Body>
-            </Tailwind>
-        </Html>
-    );
+  return (
+    <Html>
+      <Head />
+      <Tailwind>
+        <Body className="mx-auto my-auto bg-white font-sans">
+          <Container className="mx-auto my-[40px] w-[465px] rounded border border-solid border-[#eaeaea] p-[20px]">
+            <Heading className="mx-0 my-[30px] p-0 text-center text-[24px] font-normal text-black">
+              You got a message!
+            </Heading>
+            <Text className="text-[14px] leading-[24px] text-black">
+              Hello Gaurav,
+            </Text>
+            <Text className="text-[14px] leading-[24px] text-black">
+              Category: {category}
+            </Text>
+            <Text className="text-[14px] leading-[24px] text-black">
+              Tech URL: {url}
+            </Text>
+          </Container>
+        </Body>
+      </Tailwind>
+    </Html>
+  );
 };
